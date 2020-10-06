cd7d4834c79f4b912b2c6e70717263c056980e62
L-H1992 <72442994+L-H1992@users.noreply.github.com>
2020-10-06 12:24:46 +0200

    Add files via upload

---
+++ Blog.js
@@ -0,0 +1,29 @@
+
+$("#ICT3").on("mouseover",function(){
+ $("#ICT3").html("<ul><li>Digital business communication</li><li>Collaborative working</li><li>Problem solving</li> <li> Creating a digital showcase</li><li>Digital imaging and photography</li><li>Web authoring</li> </ul>");
+});
+$("#ICT3").on("mouseout",function(){
+ $("#ICT3").text("Modules studied");
+});
+
+$("#CR3").on("mouseover",function(){
+ $("#CR3").html("<ul><li>Digital graphics editing</li></li>Pre-production skills</li> <li>Digital media skills for asset production</li><li>Creating a digital sound track</li> <li>Developing an enhanced website</li></ul>");
+});
+$("#CR3").on("mouseout",function(){
+ $("#CR3").text("Modules studied");
+});
+
+$("#ICT2").on("mouseover",function(){
+ $("#ICT2").html("<ul><li>Using ICT to present information</li><li>Introduction to computer systems</li><li>Website development</li> <li>Customising applications software</li><li>Database software</li><li>Spreadsheet software</li><li>ICT graphics</li></ul");
+});
+$("#ICT2").on("mouseout",function(){
+ $("#ICT2").text("Modules studied");
+});
+
+$("#GCSE2").on("mouseover",function(){
+ $("#GCSE2").html("<ul><li>A - English Language</li><li>A - English Literature</li><li>B - Maths</li><li>B - Science</li><li>C - Additional science</li><li>A - Food Technology</li><li>B - Religious Education</li><li>C - Citizenship</li><li>C - Humanities</li></ul");
+});
+$("#GCSE2").on("mouseout",function(){
+ $("#GCSE2").text("Subjects studied");
+});
+
